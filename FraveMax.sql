195475700853293c5313588755918c7ecc77c9d3
PamelaViveros <pviveros@frba.utn.edu.ar>
2023-10-02 17:25:43 -0300

    subo ArchivoSql

---
+++ FraveMax.sql
@@ -0,0 +1,151 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost
+-- Generation Time: Oct 02, 2023 at 09:33 PM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `FraveMax`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Cliente`
+--
+
+CREATE TABLE `Cliente` (
+  `idCliente` int(11) NOT NULL,
+  `Apellido` varchar(60) NOT NULL,
+  `Nombre` varchar(60) NOT NULL,
+  `Domicilio` varchar(60) NOT NULL,
+  `Telefono` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `DetalleVenta`
+--
+
+CREATE TABLE `DetalleVenta` (
+  `idDetalleVent` int(11) NOT NULL,
+  `Cantidad` int(11) NOT NULL,
+  `idVenta` int(11) NOT NULL,
+  `PrecioVenta` double NOT NULL,
+  `idProducto` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Producto`
+--
+
+CREATE TABLE `Producto` (
+  `idProducto` int(11) NOT NULL,
+  `NombreProducto` varchar(60) NOT NULL,
+  `Descripcion` varchar(60) NOT NULL,
+  `PrecioActual` double NOT NULL,
+  `Stock` int(11) NOT NULL,
+  `Estado` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Ventas`
+--
+
+CREATE TABLE `Ventas` (
+  `idVenta` int(11) NOT NULL,
+  `idCliente` int(11) NOT NULL,
+  `FechaVent` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `Cliente`
+--
+ALTER TABLE `Cliente`
+  ADD PRIMARY KEY (`idCliente`);
+
+--
+-- Indexes for table `DetalleVenta`
+--
+ALTER TABLE `DetalleVenta`
+  ADD PRIMARY KEY (`idDetalleVent`),
+  ADD KEY `idVenta` (`idVenta`),
+  ADD KEY `idProducto` (`idProducto`);
+
+--
+-- Indexes for table `Producto`
+--
+ALTER TABLE `Producto`
+  ADD PRIMARY KEY (`idProducto`);
+
+--
+-- Indexes for table `Ventas`
+--
+ALTER TABLE `Ventas`
+  ADD PRIMARY KEY (`idVenta`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `Cliente`
+--
+ALTER TABLE `Cliente`
+  MODIFY `idCliente` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `DetalleVenta`
+--
+ALTER TABLE `DetalleVenta`
+  MODIFY `idDetalleVent` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `Producto`
+--
+ALTER TABLE `Producto`
+  MODIFY `idProducto` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `Ventas`
+--
+ALTER TABLE `Ventas`
+  MODIFY `idVenta` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `DetalleVenta`
+--
+ALTER TABLE `DetalleVenta`
+  ADD CONSTRAINT `DetalleVenta_ibfk_1` FOREIGN KEY (`idVenta`) REFERENCES `Ventas` (`idVenta`),
+  ADD CONSTRAINT `DetalleVenta_ibfk_2` FOREIGN KEY (`idProducto`) REFERENCES `Producto` (`idProducto`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
